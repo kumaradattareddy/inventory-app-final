22ac2608d0056503f6b96f7427d36b44251560c6
kumara <kumaradattareddy@gmail.com>
2025-09-19 02:17:26 +0530

    re

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,30 +1,15 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
-import { cookies } from 'next/headers';
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-export const createClient = async () => {
-  const cookieStore = await cookies();
-
+export const createClient = () => {
+  const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          try {
-            cookieStore.set(name, value, options);
-          } catch (error) {
-            // This can be ignored if you have middleware refreshing user sessions
-          }
-        },
-        remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.set(name, '', options);
-          } catch (error) {
-            // This can be ignored if you have middleware refreshing user sessions
-          }
         },
       },
     }
